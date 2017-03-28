1426ec05d1445ad769f5b268c2e89bd5bec09bcd
Yu Abigale <yupeichun@kingsoft.com>
2017-03-28 13:48:07 +0800

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,55 @@
-var gulp = require('gulp');
-var minify = require('gulp-minify');
-var clean = require('gulp-clean');
-var concat = require('gulp-concat');
-var webserver = require('gulp-webserver');
-var opn       = require('opn');
+var gulp = require('gulp');
+var minify = require('gulp-minify');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+// var webserver = require('gulp-webserver');
+const path = require('path');
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
 
-var server = {
-  host: 'localhost',
-  port: '8001'
+var server = {
+  host: 'localhost',
+  port: '8001'
 }
 
-gulp.task('default', ['clean','build', 'webserver', 'watch', 'openbrowser'],function() {
-  // 将你的默认的任务代码放在这
+gulp.task('default', ['clean'],function() {
+  gulp.start(['build', 'browser-sync', 'watch']);
+  // 将你的默认的任务代码放在这
 
 });
 
-gulp.task('clean', function () {
-    return gulp.src('build', {read: false})
-        .pipe(clean());
+gulp.task('clean', function () {
+    return gulp.src('build', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('build',['html','js'],function() {
 });
 
 
-
-gulp.task('build',['html','js'],function() {
+gulp.task('html',function() {
+    gulp.src('app/*.html') // 匹配 'app/index.html' 并且将 `base` 解析为 `app`
+    .pipe(minify())
+    .pipe(gulp.dest(path.join(__dirname, 'build')));  // 写入 'build/index.html'
 });
 
+gulp.task('js',function() {
+   
 
-gulp.task('html',function() {
-    gulp.src('app/*.html') // 匹配 'app/index.html' 并且将 `base` 解析为 `app`
-    .pipe(minify())
-    .pipe(gulp.dest('build'));  // 写入 'build/index.html'
+    gulp.src('app/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest(path.join(__dirname, 'build')));
 });
 
-gulp.task('js',function() {
-    gulp.src('app/*.js') // 匹配 'app/app.js' 并且将 `base` 解析为 `app`
-    .pipe(concat('app.js'))
-    .pipe(gulp.dest('build'));  // 写入 'build/app.js'
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
 });
 
-gulp.task('webserver', function() {
-  gulp.src( '.' )
-    .pipe(webserver({
-      host:             server.host,
-      port:             server.port,
-      livereload:       true,
-      directoryListing: false
-    }));
-});
-
-gulp.task('openbrowser', function() {
-  opn( 'http://' + server.host + ':' + server.port );
-});
-
-gulp.task('watch', function(){
-//   gulp.watch(sourcePaths.styles, ['sass']);
+gulp.task('watch', function(){
 });
